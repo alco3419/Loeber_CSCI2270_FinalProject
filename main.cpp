d2a8e6a13d5e85cfdc5d31d7181e539e3c30a0c0
Andrew Loeber <andrew.loeber@colorado.edu>
2016-04-19 18:07:35 -0600

    added the main.cpp file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string team;
+    string fn;
+    string ln;
+    string pos;
+    string born;
+    string international;
+    int played;
+    int goals;
+    int yellows;
+    int reds;
+    string status;
+
+}
